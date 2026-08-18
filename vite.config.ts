328c45453aadea33a1dcf84def7cbf054f783423
Br1anHnr <Br1anHnr@users.noreply.github.com>
2026-08-18 19:29:21 -0300

    Trigger GitHub Actions deploy with base path /valemindv2/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/valemindv2/',
+  base: process.env.GITHUB_ACTIONS ? '/valemindv2/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
